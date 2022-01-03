159df10509c4c8a17cdac76c90368da933b53f55
kaiweinan <kaiweinan@gmail.com>
2022-01-02 23:13:06 -0500

    commit tree 8e7404889f2636f893c73f6fe3b1be3de0b6b465

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+function testfunc(){
+    return "hi"
+}
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
+
+  function superbowlWin(s) {
+      let found = s.find(elem => elem.result === "W")
+      return !!found? found.year:undefined;
+  }
